c00bb10deee6da1420369a41ec4d943c22db1488
terraform <terraform@isa-server>
2022-09-17 14:07:59 +0200

    Example with CSR

---
+++ tls_private_key_and_csr/main.tf
@@ -0,0 +1,20 @@
+# Before generate key with ssh-keygen -b 4096 -t RSA
+# with output as /home/terraform/data/tls/tls_private_key_and_csr/private_key.pem
+resource "local_file" "key_data" {
+        filename       = "/home/terraform/data/tls/tls_private_key_and_csr/private_key.pem"
+        content = tls_private_key.private_key.private_key_pem
+        file_permission =  "0400"
+}
+resource "tls_private_key" "private_key" {
+  algorithm   = "RSA"
+  rsa_bits  = 4096
+}
+resource "tls_cert_request" "csr" {
+  private_key_pem = file("/home/terraform/data/tls/tls_private_key_and_csr/private_key.pem")
+  depends_on = [ local_file.key_data, tls_private_key.private_key ]
+  
+  subject {
+    common_name  = "flexit.com"
+    organization = "FlexIT Consulting Services"
+  }
+}
